e2007cd0a691edfbe745747fd7cb356ac697aae3
Farah <36897287+fr7m7moud@users.noreply.github.com>
2019-03-02 15:26:16 -0800

    starting in to do

---
+++ ios/graduation project/Views/DoneTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  DoneTableCell.swift
+//  graduation project
+//
+//  Created by farah on 3/2/19.
+//  Copyright © 2019 Ajenda. All rights reserved.
+//
+
+import UIKit
+
+class DoneTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
