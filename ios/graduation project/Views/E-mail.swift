b415e4b96cee86871b7f24cd320a66ed199ee0cf
Ahmed <ahmed@ahmeds-Mac.local>
2019-04-01 13:27:26 +0200

    email part

--- ios/graduation project/Views/E-mail.swift
+++ ios/graduation project/Views/E-mail.swift
@@ -30,10 +30,27 @@ class E_mail: UIViewController, MFMailComposeViewControllerDelegate {
     
 
     
-    
+    //check if the device can send email or it showes errors
 
     @IBAction func SendEmail(_ sender: Any) {
-        
+        if EmailFrom.text == "" || EmailTo.text == "" || EmailBody.text == "" || Subject.text == "" {
+            let alert = UIAlertController(title: "", message: "You Can't Leave Any Of These Fields Empty.", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { (action) in
+                alert.dismiss(animated: true, completion: nil)
+            }))
+            
+            alert.addAction(UIAlertAction(title: "Discard", style: .default, handler: { (action) in
+                alert.dismiss(animated: true, completion: nil)
+                self.navigationController?.popViewController(animated: true)
+            }))
+            
+            present(alert, animated: true, completion: nil)
+
+        }
+        else {
+
+
         let mailCompose = configureMailCont()
         if MFMailComposeViewController.canSendMail(){
             self.present (mailCompose, animated: true, completion: nil)
@@ -42,13 +59,12 @@ class E_mail: UIViewController, MFMailComposeViewControllerDelegate {
         showMailError()
             
         }
-       
+        }
     }
     
+    //set the email configurations
     func configureMailCont() -> MFMailComposeViewController {
-//        guard MFMailComposeViewController.canSendMail() else{
-//            return
-//        }
+ 
         let composer = MFMailComposeViewController()
         composer.mailComposeDelegate = self
         composer.setToRecipients([EmailTo.text!])
@@ -61,7 +77,7 @@ class E_mail: UIViewController, MFMailComposeViewControllerDelegate {
         }
     
     
-    
+    //show the error in alert
     func showMailError () {
         let sendMailErrorAlert = UIAlertController(title: "couldn't send Email", message: "your device couldn't send Email", preferredStyle: .alert)
         let dismiss = UIAlertAction (title: "OK", style: .default, handler: nil)
@@ -71,7 +87,7 @@ class E_mail: UIViewController, MFMailComposeViewControllerDelegate {
     }
     
     
-    
+    //result did finish with
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         
         controller.dismiss(animated: true)
