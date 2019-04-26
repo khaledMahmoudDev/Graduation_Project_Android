ec5b088cd137b6c3392e9febda83536d6219688b
fr7m7moud70 <farah.015121@ci.suez.edu.eg>
2019-04-26 23:34:01 +0200

    final- with signin and signout

--- ios/graduation project/Views/Profile.swift
+++ ios/graduation project/Views/Profile.swift
@@ -13,61 +13,21 @@ import Firebase
 
 class Profile: UIViewController {
     
-<<<<<<< HEAD
-    var reference :DatabaseReference!
-=======
     var ref: DatabaseReference!
-
->>>>>>> parent of f74227e... fetching user profile image from firebase
     
+    var reference: StorageReference!
+
+    @IBOutlet weak var profileImage: UIImageView!
     
     @IBOutlet weak var username: UILabel!
     @IBOutlet weak var email: UILabel!
     @IBOutlet weak var phone: UILabel!
-<<<<<<< HEAD
-    let userDefault = UserDefaults.standard
-=======
->>>>>>> parent of f74227e... fetching user profile image from firebase
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
-        guard let emailValue = Auth.auth().currentUser?.email else {return}
-        print(emailValue)
-        email.text = emailValue
-        
-<<<<<<< HEAD
-        
-//        //let userID = Auth.auth().currentUser?.uid
-//        reference.child("USERS").child(emailValue).observeSingleEvent(of: .value, with: { (snapshot) in
-//            // Get user value
-//            let value = snapshot.value as? NSDictionary
-//            let usernamedata = value?["username"] as? String ?? ""
-//
-//            print(usernamedata)
-//            //let user = User(username: username)
-//
-//            // ...
-//        }) { (error) in
-//            print(error.localizedDescription)
-//        }
-        
-        
-//        reference.observeSingleEvent(of: .value, with: { (snapshot) in
-//                        // Get user value
-//            print(snapshot)
-////                        let value = snapshot.value as? NSDictionary
-////                        let usernamedata = value?["username"] as? String ?? ""
-////
-////                        print(usernamedata)
-////                        //let user = User(username: username)
-////
-////                        // ...
-////                    }) { (error) in
-////                        print(error.localizedDescription)
-//                    }
-//    )}
-=======
         let userID = Auth.auth().currentUser?.uid
         ref = Database.database().reference().child("USERS").child(userID!)
         
@@ -85,40 +45,32 @@ class Profile: UIViewController {
         }) { (error) in
             print(error.localizedDescription)
         }
+        
+        
+        let storageRef = Storage.storage().reference(forURL: "gs://ajenda-a702f.appspot.com/").child("profileImages").child(userID!)
+        
+        storageRef.downloadURL { url, error in
+            if let error = error {
+                // Handle any errors
+                print(error)
+            } else {
+                // Get the download URL for 'images/stars.jpg'
+//                                let UrlString = url!.absoluteString
+//                                print(UrlString)
+                
+                let data = NSData(contentsOf: url!)
+                let image = UIImage(data: data! as Data)
+                self.profileImage.image = image
+            }
+        }
   
     }
-    
->>>>>>> parent of f74227e... fetching user profile image from firebase
-
-     // your ref ie. root.child("users").child("stephenwarren001@yahoo.com")
-    
-    // only need to fetch once so use single event
-    
-//    reference.observeSingleEventOfType(.Value, withBlock: { snapshot in
-//
-//    if !snapshot.exists() { return }
-//
-//    //print(snapshot)
-//
-//    if let userName = snapshot.value["username"] as? String {
-//    print(userName)
-//    }
-//    if let email = snapshot.value["email"] as? String {
-//    print(email)
-//    }
-//
-//    // can also use
-//    // snapshot.childSnapshotForPath("full_name").value as! String
-//    })
-
-
-    }
-    @IBAction func logOut(_ sender: Any) {
-
+  
+    @IBAction func LogoutBtn(_ sender: Any) {
+        
         do {
             try Auth.auth().signOut()
-            userDefault.removeObject(forKey: "signedIn")
-            userDefault.synchronize()
+            
             let login = self.storyboard?.instantiateViewController(withIdentifier: "LoginNavigation") as! LoginNavigation
             let appDelegate = UIApplication.shared.delegate
             appDelegate?.window??.rootViewController = login
@@ -128,5 +80,10 @@ class Profile: UIViewController {
         }
         
     }
-    
 }
+
+
+
+
+    
+
