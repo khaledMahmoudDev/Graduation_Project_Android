c8764b73943ced39306d1ec2fa20b6c85a06fadd
ahmed mostafa <ahmedmosthalim>
2019-06-15 01:41:57 +0200

    ToDoList
    todo table view
    todo details
    done table view
    done details

---
+++ ios/graduation project/Views/DoneDetails.swift
@@ -0,0 +1,34 @@
+//
+//  DoneDetails.swift
+//  graduation project
+//
+//  Created by ahmed on 6/15/19.
+//  Copyright © 2019 Ajenda. All rights reserved.
+//
+
+import UIKit
+
+class DoneDetails: UIViewController {
+
+    @IBAction func OKDone(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ios/graduation project/Views/ToDoCellDetails.swift
@@ -0,0 +1,76 @@
+//
+//  ToDoCellDetails.swift
+//  graduation project
+//
+//  Created by ahmed on 6/14/19.
+//  Copyright © 2019 Ajenda. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ToDoCellDetails: UIViewController {
+
+    @IBOutlet weak var ToDoTitle: UITextField!
+    @IBOutlet weak var ToDoDetail: UITextView!
+    @IBOutlet weak var ToDoCategory: UITextField!
+    
+    
+    
+    //save button to save a todo in the table view
+    
+    @IBAction func Save(_ sender: Any) {
+        if (ToDoTitle.text != "" && ToDoDetail.text != "" && ToDoCategory.text != ""){
+            
+            var title = ToDoTitle.text
+            var details = ToDoDetail.text
+            var cat = ToDoCategory.text
+            
+            list.append(ToDoTitle.text!)
+            ToDoTitle.text = ""
+            ToDoDetail.text = ""
+            ToDoCategory.text = ""
+            
+            
+            //save the category in category label in todo table
+            
+        }
+        else {
+            let alert = UIAlertController(title: "", message: "Please,fill in all the fields.", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action) in
+                alert.dismiss(animated: true, completion: nil)
+            }))
+            
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+        
+      
+    }
+    @IBAction func MoveToDone(_ sender: Any) {
+        
+        DoneList.append(ToDoTitle.text!)
+        
+
+    }
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        ToDoCategory.addTarget(self, action: showCats, for: UITouch)
+//        func showCats (textfield: UITextField)
+//        { // show the cats table}
+//            
+            
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
